c49da2e02acd3561d28ca000d80e05f4a79de7a9
lainleaflet <if.danhlain@gmail.com>
2025-06-20 17:12:45 -0400

    created separate map component

---
+++ client/src/Map.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+import axios from "axios"
+import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
+import MapClickHandler from './MapClickHandler';
+
+function Map ({location}) {
+    const [guess, setGuess] = useState(null)
+
+    return (
+    <div>
+      {location && (
+        <MapContainer
+          center = {[20, 0]}
+          zoom={5}
+          style={{ height: "320px", width: "400px" }}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+          <MapClickHandler onGuess = {setGuess} />
+
+          {guess && (
+            <Marker position={guess}>
+              <Popup>Your guess</Popup>
+            </Marker>
+          )}
+
+          {location && (
+            <Marker position={[location.lat, location.lng]}>
+              <Popup>Actual Location</Popup>
+            </Marker>
+          )}
+        </MapContainer>
+      )}
+    </div>
+    )
+}
+
+export default Map
