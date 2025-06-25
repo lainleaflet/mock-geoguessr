0210d76dd57bb5e1a7b2ecac63bc27b03e5557ce
lainleaflet <if.danhlain@gmail.com>
2025-06-25 16:53:03 -0400

    implemented streetview api

---
+++ client/src/StreetView.jsx
@@ -0,0 +1,31 @@
+import { LoadScript, GoogleMap, StreetViewPanorama} from "@react-google-maps/api"
+
+const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+const containerStyle = {
+  width: "100%",
+  height: "100vh"
+}
+
+function StreetView ({location}) {
+    return (
+        <LoadScript googleMapsApiKey={API_KEY}>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={location}
+                zoom={12}
+            >
+                <StreetViewPanorama
+                    position={location}
+                    visible={true}
+                    options={{
+                        disableDefaultUI: true,
+                        showRoadLabels: false
+                    }}
+                />
+            </GoogleMap>
+        </LoadScript>
+    )
+}
+
+export default StreetView;
