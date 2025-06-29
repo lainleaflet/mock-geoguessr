90f0dcb2635a84c99a3d57eed9d442bdc9b51f3b
lainleaflet <if.danhlain@gmail.com>
2025-06-29 11:00:57 -0400

    moved location-getting to frontend

--- server/app.py
+++ server/app.py
@@ -4,12 +4,5 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/random-location")
-def random_location():
-    return jsonify({
-        "lat": 39.482231,  #coordinates of breckenridge, colorado for now
-        "lng": -106.046181   
-    })
-
 if __name__ == "__main__":
     app.run(debug=True)
