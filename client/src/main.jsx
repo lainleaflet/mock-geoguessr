5195cb1dcbb7976e9ea8d3dd504e20febbc478f1
lainleaflet <if.danhlain@gmail.com>
2025-06-29 10:58:39 -0400

    added api loading before rendering app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,16 @@
 import "leaflet/dist/leaflet.css";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { LoadScript } from "@react-google-maps/api";
 import './index.css'
 import App from './App.jsx'
 
+const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode> cancelled just so api calls are not loaded twice
+  <LoadScript googleMapsApiKey={API_KEY}>
     <App />
-  </StrictMode>,
+  </LoadScript>
+  // {/* </StrictMode>, */}
 )
